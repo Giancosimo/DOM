021e4ee82b6470a68e768b38980ab5549d0e1e4b
[Giancosimo] <[giancx91@gmail.com]>
2024-09-20 17:58:41 +0200

    DOM ex

--- ex5/ex5.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector(".btn");
-
-button.addEventListener("click", (event) => {
-  console.log("button event", event);
-  const body = document.querySelector("body");
-  body.classList.toggle("theme");
-});
